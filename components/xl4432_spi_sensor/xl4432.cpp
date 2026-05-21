fc2fdbd76363be52cb91aecceb202bc97baa131b
ShonP40 <13995143+ShonP40@users.noreply.github.com>
2026-05-21 20:43:03 +0300

    Fix v2

--- components/xl4432_spi_sensor/xl4432.cpp
+++ components/xl4432_spi_sensor/xl4432.cpp
@@ -169,6 +169,10 @@ PacketStatus Xl4432::validatePacket()
 
 	// 2-bit correction
 	for (int i = 0; i < NUM_TOTAL; i++) {
+		if ((i & 0x0F) == 0) {
+			App.feed_wdt();
+			yield();
+		}
 		uint64_t r1 = syndrome ^ syn[i];
 		for (int j = i+1; j < NUM_TOTAL; j++) {
 			if (syn[j] == r1) {
@@ -180,6 +184,10 @@ PacketStatus Xl4432::validatePacket()
 
 	// 3-bit correction (~680K iterations worst case)
 	for (int i = 0; i < NUM_TOTAL; i++) {
+		if ((i & 0x07) == 0) {
+			App.feed_wdt();
+			yield();
+		}
 		uint64_t r1 = syndrome ^ syn[i];
 		for (int j = i+1; j < NUM_TOTAL; j++) {
 			uint64_t r2 = r1 ^ syn[j];
